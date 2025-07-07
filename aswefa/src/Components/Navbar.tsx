6d053a20452d9086aa2597ca7b61ad44cada1bb8
yoojanghun <yoojanghun@naver.com>
2025-07-07 16:02:22 +0900

    nav bar에 logo 추가

--- aswefa/src/Components/Navbar.tsx
+++ aswefa/src/Components/Navbar.tsx
@@ -1,8 +1,10 @@
 import { Link } from "react-router-dom";
+import logo from "../assets/logo.png";
 
 function Navbar() {
     return (
         <nav style={{ display: "flex", flexDirection: "column", width: "200px" }}>
+            <Link to="/"><img src={logo} alt="logo" /></Link>
             <Link to="/">운행 정보</Link>
             <Link to="/location">위치 조회</Link>
             <Link to="/management">차량 관리</Link>
