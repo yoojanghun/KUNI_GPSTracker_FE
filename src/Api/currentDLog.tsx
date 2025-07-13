5ea541b3c02c43325ee01055e50cafd2e620d7bd
lee <iamlyg9667@kookmin.ac.kr>
2025-07-14 02:07:27 +0900

    [FEAT]: 운행일지 전역상태 추가 및 더미데이터 추가

---
+++ src/Api/currentDLog.tsx
@@ -0,0 +1,402 @@
+export const currentDLog = [
+  {
+    "carNumber": "65사2860",
+    "carName": "K3",
+    "startTime": "2025-07-08T12:55:00",
+    "endTime": "2025-07-08T13:11:00",
+    "distance": 102,
+    "driver": "김수빈"
+  },
+  {
+    "carNumber": "88사9798",
+    "carName": "Avante",
+    "startTime": "2025-07-08T12:06:00",
+    "endTime": "2025-07-08T12:33:00",
+    "distance": 146,
+    "driver": "박하늘"
+  },
+  {
+    "carNumber": "28아7725",
+    "carName": "Avante",
+    "startTime": "2025-07-08T12:55:00",
+    "endTime": "2025-07-08T13:13:00",
+    "distance": 119,
+    "driver": "정수빈"
+  },
+  {
+    "carNumber": "63마2280",
+    "carName": "Sonata",
+    "startTime": "2025-07-08T12:59:00",
+    "endTime": "2025-07-08T13:19:00",
+    "distance": 117,
+    "driver": "신민수"
+  },
+  {
+    "carNumber": "93하0854",
+    "carName": "Tucson",
+    "startTime": "2025-07-08T12:07:00",
+    "endTime": "2025-07-08T13:02:00",
+    "distance": 150,
+    "driver": "윤지훈"
+  },
+  {
+    "carNumber": "32나9236",
+    "carName": "Genesis G70",
+    "startTime": "2025-07-08T12:05:00",
+    "endTime": "2025-07-08T12:45:00",
+    "distance": 141,
+    "driver": "박지훈"
+  },
+  {
+    "carNumber": "99가9239",
+    "carName": "K5",
+    "startTime": "2025-07-08T12:55:00",
+    "endTime": "2025-07-08T13:46:00",
+    "distance": 141,
+    "driver": "윤민수"
+  },
+  {
+    "carNumber": "48아7325",
+    "carName": "Tucson",
+    "startTime": "2025-07-08T13:00:00",
+    "endTime": "2025-07-08T13:17:00",
+    "distance": 124,
+    "driver": "황현우"
+  },
+  {
+    "carNumber": "48바9347",
+    "carName": "Tucson",
+    "startTime": "2025-07-08T12:10:00",
+    "endTime": "2025-07-08T13:01:00",
+    "distance": 145,
+    "driver": "김지아"
+  },
+  {
+    "carNumber": "80자3796",
+    "carName": "K5",
+    "startTime": "2025-07-08T12:08:00",
+    "endTime": "2025-07-08T12:45:00",
+    "distance": 145,
+    "driver": "서지훈"
+  },
+  {
+    "carNumber": "34라2596",
+    "carName": "K3",
+    "startTime": "2025-07-08T12:53:00",
+    "endTime": "2025-07-08T13:53:00",
+    "distance": 115,
+    "driver": "한예은"
+  },
+  {
+    "carNumber": "23라3222",
+    "carName": "K3",
+    "startTime": "2025-07-08T12:31:00",
+    "endTime": "2025-07-08T13:11:00",
+    "distance": 107,
+    "driver": "박지아"
+  },
+  {
+    "carNumber": "81사8682",
+    "carName": "Genesis G70",
+    "startTime": "2025-07-08T12:14:00",
+    "endTime": "2025-07-08T12:55:00",
+    "distance": 141,
+    "driver": "한현우"
+  },
+  {
+    "carNumber": "64사9588",
+    "carName": "K5",
+    "startTime": "2025-07-08T12:30:00",
+    "endTime": "2025-07-08T13:19:00",
+    "distance": 124,
+    "driver": "장채원"
+  },
+  {
+    "carNumber": "21하3802",
+    "carName": "Tucson",
+    "startTime": "2025-07-08T12:57:00",
+    "endTime": "2025-07-08T13:41:00",
+    "distance": 139,
+    "driver": "신도윤"
+  },
+  {
+    "carNumber": "98하9935",
+    "carName": "K3",
+    "startTime": "2025-07-08T12:47:00",
+    "endTime": "2025-07-08T13:05:00",
+    "distance": 139,
+    "driver": "김도윤"
+  },
+  {
+    "carNumber": "09아1314",
+    "carName": "K5",
+    "startTime": "2025-07-08T12:40:00",
+    "endTime": "2025-07-08T13:17:00",
+    "distance": 136,
+    "driver": "신시우"
+  },
+  {
+    "carNumber": "38라6112",
+    "carName": "Avante",
+    "startTime": "2025-07-08T12:52:00",
+    "endTime": "2025-07-08T13:26:00",
+    "distance": 106,
+    "driver": "이지훈"
+  },
+  {
+    "carNumber": "74사5541",
+    "carName": "Tucson",
+    "startTime": "2025-07-08T12:32:00",
+    "endTime": "2025-07-08T13:29:00",
+    "distance": 124,
+    "driver": "신준호"
+  },
+  {
+    "carNumber": "50마9964",
+    "carName": "Carnival",
+    "startTime": "2025-07-08T12:07:00",
+    "endTime": "2025-07-08T12:23:00",
+    "distance": 112,
+    "driver": "장수빈"
+  },
+  {
+    "carNumber": "86가7324",
+    "carName": "Genesis G70",
+    "startTime": "2025-07-08T12:00:00",
+    "endTime": "2025-07-08T12:19:00",
+    "distance": 115,
+    "driver": "박시우"
+  },
+  {
+    "carNumber": "65사0026",
+    "carName": "Sorrento",
+    "startTime": "2025-07-08T12:11:00",
+    "endTime": "2025-07-08T12:58:00",
+    "distance": 133,
+    "driver": "권시우"
+  },
+  {
+    "carNumber": "81라8562",
+    "carName": "K5",
+    "startTime": "2025-07-08T12:22:00",
+    "endTime": "2025-07-08T12:27:00",
+    "distance": 133,
+    "driver": "최지훈"
+  },
+  {
+    "carNumber": "46바5645",
+    "carName": "Tucson",
+    "startTime": "2025-07-08T12:52:00",
+    "endTime": "2025-07-08T13:36:00",
+    "distance": 142,
+    "driver": "윤하늘"
+  },
+  {
+    "carNumber": "89가3768",
+    "carName": "Sorrento",
+    "startTime": "2025-07-08T12:43:00",
+    "endTime": "2025-07-08T13:11:00",
+    "distance": 101,
+    "driver": "김서연"
+  },
+  {
+    "carNumber": "98나2974",
+    "carName": "K5",
+    "startTime": "2025-07-08T12:21:00",
+    "endTime": "2025-07-08T13:06:00",
+    "distance": 125,
+    "driver": "신지아"
+  },
+  {
+    "carNumber": "87마9677",
+    "carName": "K3",
+    "startTime": "2025-07-08T12:46:00",
+    "endTime": "2025-07-08T13:30:00",
+    "distance": 122,
+    "driver": "윤지민"
+  },
+  {
+    "carNumber": "36마2817",
+    "carName": "Carnival",
+    "startTime": "2025-07-08T12:15:00",
+    "endTime": "2025-07-08T12:30:00",
+    "distance": 126,
+    "driver": "오도윤"
+  },
+  {
+    "carNumber": "86하4107",
+    "carName": "K3",
+    "startTime": "2025-07-08T12:14:00",
+    "endTime": "2025-07-08T13:11:00",
+    "distance": 141,
+    "driver": "조다은"
+  },
+  {
+    "carNumber": "81사3818",
+    "carName": "Genesis G70",
+    "startTime": "2025-07-08T12:32:00",
+    "endTime": "2025-07-08T13:30:00",
+    "distance": 102,
+    "driver": "권지민"
+  },
+  {
+    "carNumber": "92아0708",
+    "carName": "Genesis G70",
+    "startTime": "2025-07-08T12:30:00",
+    "endTime": "2025-07-08T13:13:00",
+    "distance": 118,
+    "driver": "윤다은"
+  },
+  {
+    "carNumber": "61하7613",
+    "carName": "Sonata",
+    "startTime": "2025-07-08T12:00:00",
+    "endTime": "2025-07-08T12:25:00",
+    "distance": 133,
+    "driver": "서시우"
+  },
+  {
+    "carNumber": "36가3394",
+    "carName": "Sonata",
+    "startTime": "2025-07-08T12:23:00",
+    "endTime": "2025-07-08T12:29:00",
+    "distance": 116,
+    "driver": "한지민"
+  },
+  {
+    "carNumber": "24사5637",
+    "carName": "Sonata",
+    "startTime": "2025-07-08T12:44:00",
+    "endTime": "2025-07-08T12:53:00",
+    "distance": 100,
+    "driver": "이예은"
+  },
+  {
+    "carNumber": "11라3804",
+    "carName": "Genesis G70",
+    "startTime": "2025-07-08T12:13:00",
+    "endTime": "2025-07-08T12:39:00",
+    "distance": 106,
+    "driver": "황수빈"
+  },
+  {
+    "carNumber": "59라2460",
+    "carName": "Avante",
+    "startTime": "2025-07-08T12:57:00",
+    "endTime": "2025-07-08T13:14:00",
+    "distance": 131,
+    "driver": "한지민"
+  },
+  {
+    "carNumber": "81자7645",
+    "carName": "Avante",
+    "startTime": "2025-07-08T12:03:00",
+    "endTime": "2025-07-08T12:24:00",
+    "distance": 149,
+    "driver": "신현우"
+  },
+  {
+    "carNumber": "34하6056",
+    "carName": "Sonata",
+    "startTime": "2025-07-08T12:02:00",
+    "endTime": "2025-07-08T12:56:00",
+    "distance": 137,
+    "driver": "한지민"
+  },
+  {
+    "carNumber": "03아1865",
+    "carName": "Carnival",
+    "startTime": "2025-07-08T12:09:00",
+    "endTime": "2025-07-08T12:48:00",
+    "distance": 102,
+    "driver": "권지민"
+  },
+  {
+    "carNumber": "67가3859",
+    "carName": "Avante",
+    "startTime": "2025-07-08T12:35:00",
+    "endTime": "2025-07-08T12:53:00",
+    "distance": 137,
+    "driver": "박준호"
+  },
+  {
+    "carNumber": "26라8816",
+    "carName": "K3",
+    "startTime": "2025-07-08T12:27:00",
+    "endTime": "2025-07-08T13:25:00",
+    "distance": 127,
+    "driver": "최예은"
+  },
+  {
+    "carNumber": "81가7644",
+    "carName": "Sonata",
+    "startTime": "2025-07-08T12:27:00",
+    "endTime": "2025-07-08T12:49:00",
+    "distance": 133,
+    "driver": "이지민"
+  },
+  {
+    "carNumber": "23바0867",
+    "carName": "Carnival",
+    "startTime": "2025-07-08T12:30:00",
+    "endTime": "2025-07-08T12:37:00",
+    "distance": 130,
+    "driver": "박서연"
+  },
+  {
+    "carNumber": "20가3058",
+    "carName": "K5",
+    "startTime": "2025-07-08T12:57:00",
+    "endTime": "2025-07-08T13:53:00",
+    "distance": 140,
+    "driver": "윤지훈"
+  },
+  {
+    "carNumber": "87아3342",
+    "carName": "Tucson",
+    "startTime": "2025-07-08T12:34:00",
+    "endTime": "2025-07-08T12:47:00",
+    "distance": 143,
+    "driver": "정수빈"
+  },
+  {
+    "carNumber": "25아3108",
+    "carName": "Carnival",
+    "startTime": "2025-07-08T12:04:00",
+    "endTime": "2025-07-08T12:11:00",
+    "distance": 105,
+    "driver": "박지민"
+  },
+  {
+    "carNumber": "76사7957",
+    "carName": "Sorrento",
+    "startTime": "2025-07-08T12:48:00",
+    "endTime": "2025-07-08T13:42:00",
+    "distance": 140,
+    "driver": "서유진"
+  },
+  {
+    "carNumber": "52다2510",
+    "carName": "Genesis G70",
+    "startTime": "2025-07-08T12:17:00",
+    "endTime": "2025-07-08T12:53:00",
+    "distance": 148,
+    "driver": "서준호"
+  },
+  {
+    "carNumber": "83하5283",
+    "carName": "Sonata",
+    "startTime": "2025-07-08T12:39:00",
+    "endTime": "2025-07-08T13:38:00",
+    "distance": 106,
+    "driver": "장시우"
+  },
+  {
+    "carNumber": "82가3958",
+    "carName": "K5",
+    "startTime": "2025-07-08T12:35:00",
+    "endTime": "2025-07-08T13:01:00",
+    "distance": 116,
+    "driver": "권수빈"
+  }
+]
