47839a5db5b7e4de63c13006941b9df218e918ed
yoojanghun <yoojanghun@naver.com>
2025-07-15 16:19:10 +0900

    차량별 클러스터링 버튼

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -3,81 +3,134 @@ import working from "../../assets/car-status-icons/working.png";
 import notWorking from "../../assets/car-status-icons/not-working.png";
 import checkingIndicator from "../../assets/car-status-icons/checking-indicator.png";
 import notWorkingIndicator from "../../assets/car-status-icons/not-working-indicator.png";
+import workingIndicator from "../../assets/car-status-icons/working-indicator.png";
+import location from "../../assets/nav-bar-icons/location_on.png";
 import styles from "./Home.module.css";
 
 import SimpleLineChart from "@/Components/chart";
 import MapTest from "@/Components/map";
+import { useState, type ChangeEvent } from "react";
 
 
 function Home() {
     const percentage = 30;
+    const [carStatusBtn, setCarStatusBtn] = useState("전체");
+
+    function handleCarStatusBtn(e: ChangeEvent<HTMLInputElement>) {
+        setCarStatusBtn(e.target.value);
+    }
 
     return (
         <main className="flex-1 box-border p-5">
-            <div className="flex gap-3 mb-5">
-                <div className={`${styles["icon"]} flex flex-1 justify-center items-center border rounded-xl p-[23px] min-w-[202px] max-h-[90px]`}>
-                    <img className="w-10 h-10 mr-2" src={total} alt="전체 차량 아이콘" />
+            <div className="flex gap-5 mb-5">
+                <div className={`${styles["icon"]} flex flex-1 justify-center items-center border rounded-xl p-[23px] min-w-[202px] min-h-[110px]`}>
+                    <img className="w-13 h-13 mr-3" src={total} alt="전체 차량 아이콘" />
                     <div>
                         <div>
-                            <span className="text-xl font-bold mr-2">2000</span>
-                            <span className="text-xs opacity-60">전체 차량</span>
+                            <span className="text-3xl font-bold mr-2">2000</span>
+                            <span className="text-sm opacity-60">전체 차량</span>
                         </div>
                         {/* <div>progress bar 여긴 없음</div> */}
                     </div>
                 </div>
 
-                <div className={`${styles["icon"]} flex flex-2 justify-center items-center border rounded-xl p-[23px] max-h-[90px]`}>
-                    <div className="flex items-center min-w-[230px]">
-                        <img className="w-10 h-10 mr-2" src={notWorking} alt="미운행 차량 아이콘"/>
+                <div className={`${styles["icon"]} flex flex-2 justify-center items-center border rounded-xl p-[23px] min-h-[110px]`}>
+                    <div className="flex items-center min-w-[280px]">
+                        <img className="w-13 h-13 mr-3" src={notWorking} alt="미운행 차량 아이콘"/>
                         <div>
-                            <div>
-                                <span className="text-xl font-bold mr-2">{percentage}%</span>
-                                <span className="text-xs opacity-60">미운행 차량 (1,001)</span>
+                            <div className="mr-6">
+                                <span className="text-3xl font-bold mr-3">{percentage}%</span>
+                                <span className="text-sm opacity-60">미운행 차량 (1,001)</span>
                             </div>
-                            <div className="w-40 h-2 mt-2 bg-gray-200 rounded-full overflow-hidden">
+                            <div className="w-50 h-2 mt-2 bg-gray-200 rounded-full overflow-hidden">
                                 <div className="h-full bg-red-500" style={{ width: `${percentage}%` }} />
                             </div>
                         </div>
                     </div>
-                    <div className="min-w-[160px]">
+                    <div className="min-w-[180px]">
                         <div className="flex items-center mb-2">
-                            <img className="w-6 h-6 mr-1" src={checkingIndicator} alt="점검중 차량 아이콘" />
+                            <img className="w-8 h-8 mr-1" src={checkingIndicator} alt="점검중 차량 아이콘" />
                             <div>
-                                <span className="text-xs font-bold mr-1">점검중인 차량</span>
-                                <span className="text-[10px] opacity-60">301대</span>
+                                <span className="font-bold mr-1">점검중인 차량</span>
+                                <span className="text-[14px] opacity-60">301대</span>
                             </div>
                         </div>
                         <div className="flex items-center">
-                            <img className="w-6 h-6 mr-1" src={notWorkingIndicator} alt="미운행 차량 아이콘" />
+                            <img className="w-8 h-8 mr-1" src={notWorkingIndicator} alt="미운행 차량 아이콘" />
                             <div>
-                                <span className="text-xs font-bold mr-1">미운행 차량</span>
-                                <span className="text-[10px] opacity-60">700대</span>
+                                <span className="font-bold mr-1">미운행 차량</span>
+                                <span className="text-[14px] opacity-60">700대</span>
                             </div>
                         </div>
                     </div>
                     <div className="flex flex-col items-center">
-                        <span className="text-xl font-bold mb-1">100%</span>
-                        <span className="text-xs opacity-60">가동률</span>
+                        <span className="text-3xl font-bold mb-1">100%</span>
+                        <span className="opacity-60">가동률</span>
                     </div>
                 </div>
             </div>
             <div className="flex gap-4 h-[80%]">
                 <div className="flex flex-col flex-2 gap-5">
-                    <div className={`${styles["icon"]} flex justify-center items-center border rounded-xl p-[23px] max-h-[90px]`}>
-                        <img className="w-10 h-10 mr-2" src={working} alt="운행 차량 아이콘" />
+                    <div className={`${styles["icon"]} flex justify-center items-center border rounded-xl p-[23px] min-h-[110px]`}>
+                        <img className="w-13 h-13 mr-3" src={working} alt="운행 차량 아이콘" />
                         <div>
                             <div>
-                                <span className="text-xl font-bold mr-2">{100 - percentage}%</span>
-                                <span className="text-xs opacity-60">운행 중 차량 (999)</span>
+                                <span className="text-3xl font-bold mr-2">{100 - percentage}%</span>
+                                <span className="text-sm opacity-60">운행 중 차량 (999)</span>
                             </div>
-                            <div className="w-40 h-2 mt-2 bg-gray-200 rounded-full overflow-hidden">
+                            <div className="w-50 h-2 mt-2 bg-gray-200 rounded-full overflow-hidden">
                                 <div className="h-full bg-blue-500" style={{ width: `${100 - percentage}%` }} />
                             </div>
                         </div>
                     </div>
-                    <SimpleLineChart />
+                    <div className={`${styles["icon"]} border box-border pr-4 pt-4 pb-4 h-[100%] rounded-xl`}>
+                        <SimpleLineChart />
+                    </div>
                 </div>
-                <MapTest flexSize={3} />
+                <div className={`${styles["icon"]} flex-3 border box-border p-4 pt-2 h-[100%] rounded-3xl`}>
+                    <div className="w-[100%] h-[9%] flex items-center justify-between">
+                        <div className="flex items-center">
+                            <img src={location} className="w-7 h-7 mr-2"/>
+                            <span className="font-bold text-xl">차량 현황</span>
+                        </div>
+                        <div className="flex justify-around w-[70%]">
+                            <label className="flex items-center font-bold">
+                                <input type="checkbox" value="전체"
+                                       onChange={handleCarStatusBtn}
+                                       checked={carStatusBtn === "전체"} 
+                                       className="w-4 h-4 mr-1"/>
+                                전체
+                            </label>
+                            <label className="flex items-center font-bold">
+                                <input type="checkbox" value="점검중"
+                                       onChange={handleCarStatusBtn}
+                                       checked={carStatusBtn === "점검중"}
+                                       className="w-4 h-4 mr-1"/>
+                                <img src={checkingIndicator} className="mr-1 w-6" />
+                                점검중
+                            </label>
+                            <label className="flex items-center font-bold">
+                                <input type="checkbox" value="미운행" 
+                                       onChange={handleCarStatusBtn}
+                                       checked={carStatusBtn === "미운행"}
+                                       className="w-4 h-4 mr-1"/>
+                                <img src={notWorkingIndicator} className="mr-1 w-6" />
+                                미운행
+                            </label>
+                            <label className="flex items-center font-bold">
+                                <input type="checkbox" value="운행중" 
+                                       onChange={handleCarStatusBtn}
+                                       checked={carStatusBtn === "운행중"}
+                                       className="w-4 h-4 mr-1"/>
+                                <img src={workingIndicator} className="mr-1 w-6" />
+                                운행중
+                            </label>
+                        </div>
+                    </div>
+                    <div className="w-[100%] h-[91%]">
+                        <MapTest carStatus={carStatusBtn} />
+                    </div>
+                </div>
             </div>
         </main>
     );
