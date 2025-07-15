157eda8de5847686aff55af52bc8e00bcaf0ebbc
lee <iamlyg9667@kookmin.ac.kr>
2025-07-15 18:12:04 +0900

    [FEAT]: LogTable 날짜 입력 유효성 처리 추가

--- src/Components/DLogComponents/LogSearchButton.tsx
+++ src/Components/DLogComponents/LogSearchButton.tsx
@@ -1,20 +1,36 @@
 import { Button } from "@/Components/ui/button";
-import { Search } from "lucide-react";
+import { CircleAlert, Search } from "lucide-react";
 import { useDLogStore } from "@/Store/dlogStore";
+import { toast } from "sonner";
 
 export function LogSearchButton() {
   const applyFilter = useDLogStore((state) => state.applyFilter);
+  const filteredDLogs = useDLogStore((state) => state.filteredDLogs);
+  const { carNumber, startTime, endTime } = useDLogStore(
+    (state) => state.filter
+  );
 
   return (
     <div className="flex flex-col">
       <span className="text-transparent">-</span>
       <Button
-      onClick={applyFilter}
-      className="bg-[#000000] gap-3 hover:bg-[#000000]/80"
-    >
-      <Search strokeWidth={3} size={20} /> 검색
-    </Button>
+        onClick={() => {
+          if (!carNumber && !startTime && !endTime) {
+            toast("검색값을 입력해 주세요", {
+              icon: <CircleAlert />,
+            });
+          }
+          applyFilter();
+          if (filteredDLogs.length <= 0) {
+            toast("일치하는 기록이 없습니다", {
+              icon: <CircleAlert />,
+            });
+          }
+        }}
+        className="bg-[#000000] gap-3 hover:bg-[#000000]/80"
+      >
+        <Search strokeWidth={3} size={20} /> 검색
+      </Button>
     </div>
-    
   );
 }
--- src/Components/DLogComponents/StartDateSelection.tsx
+++ src/Components/DLogComponents/StartDateSelection.tsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react"
 import { CalendarDays } from "lucide-react"
 import { useDLogStore } from "@/Store/dlogStore"
+import { cn } from "@/lib/utils"
 
 import { Button } from "@/Components/ui/button"
 import { Calendar } from "@/Components/ui/calendar"
@@ -17,10 +18,19 @@ export function StartDateSelection() {
 
   const filter = useDLogStore((state) => state.filter );
   const setFilter = useDLogStore((state) => state.setFilter );
+  const isDateValid = useDLogStore((state) => state.isDateValid);
+  const setIsDateValid = useDLogStore((state) => state.setIsDateValid);
+
+  // 페이지 로드 시 true로 다시 초기화
+  useEffect(() => {
+  setIsDateValid(true);
+}, []);
+
 
   useEffect(() => {
     if (date) {
-      const formatted = date.toISOString().slice(0, 10)
+      const formatted = date.toLocaleDateString("sv-SE"); // "YYYY-MM-DD" 형식 보장
+      console.log('start-formatted: ', formatted);
       setFilter({ ...filter, startTime: formatted })
     }
   }, [date])
@@ -28,15 +38,23 @@ export function StartDateSelection() {
 
   return (
     <div>
-      <Label htmlFor="date" className="px-1 py-1 text-[#9E9E9E]">
-        시작 날짜
-      </Label>
+      { isDateValid || date 
+      ? <Label htmlFor="date" className="px-1 py-1 text-[#9E9E9E]">
+        시작 날짜 
+        </Label>
+      : <Label htmlFor="date" className="px-1 py-1 text-red-500">
+        날짜를 입력하세요 
+        </Label>
+      }
+      
       <Popover open={open} onOpenChange={setOpen}>
         <PopoverTrigger asChild>
           <Button
             variant="outline"
             id="date"
-            className="w-48 justify-between font-normal"
+            className={cn("w-48 justify-between font-normal", 
+              !isDateValid && !date && "border-red-500"
+            ) }
           >
             {date ? date.toLocaleDateString() : "YYYY-MM-DD"}
             <CalendarDays />
