a1ffe3a9aecba0306d70093d3ebf8c577b510cb4
lee <iamlyg9667@kookmin.ac.kr>
2025-08-19 02:49:46 +0900

    [FIX]: 빌드 오류 해결

--- src/AppLayout.tsx
+++ src/AppLayout.tsx
@@ -1,5 +1,5 @@
-import { AppSidebar } from "@/components/app-sidebar";
-import { SidebarProvider } from "@/components/ui/sidebar";
+import { AppSidebar } from "./components/app-sidebar";
+import { SidebarProvider } from "./components/ui/sidebar";
 
 export function AppLayout({ children }: { children: React.ReactNode }) {
   return (
