ff7efc3edc10476ee2573db12433d3fffaddcdf5
lee <iamlyg9667@kookmin.ac.kr>
2025-08-19 03:08:32 +0900

    [FIX]: 젠킨스 빌드 오류수정 4

--- src/AppLayout.tsx
+++ src/AppLayout.tsx
@@ -1,4 +1,4 @@
-import { AppSidebar } from "@/components/appSidebar";
+import { AppSidebar } from "@/components/appSidebar.tsx";
 import { SidebarProvider } from "@/components/ui/sidebar";
 
 export function AppLayout({ children }: { children: React.ReactNode }) {
