dd4f7bfb65e09927b3926058c012e669b2ddedd0
lee <iamlyg9667@kookmin.ac.kr>
2025-08-19 16:25:38 +0900

    [FIX]: sidebar 펼쳤을 때 나오는 로고에 홈페이지 네비게이션 추가

--- src/components/logoMenu.tsx
+++ src/components/logoMenu.tsx
@@ -11,9 +11,11 @@ import {
   SidebarMenuItem,
   useSidebar,
 } from "@/components/ui/sidebar"
+import { useNavigate } from "react-router-dom"
 
 export function LogoMenu() {
   const { toggleSidebar } = useSidebar()
+  const navigate = useNavigate();
 
   return (
     <SidebarMenu>
@@ -24,15 +26,18 @@ export function LogoMenu() {
               size="lg"
               className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
             >
-              <div className="flex items-center justify-center ml-1">
+              <div className="flex items-center justify-center ml-1 cursor-pointer">
                 <Menu onClick={()=>toggleSidebar()} size={24}/>
               </div>
-              <div className="flex aspect-square size-8 items-center justify-center rounded-lg">
+              <div className="flex items-center justify-center gap-1 cursor-pointer hover:text-[#000000]/60" onClick={() => { navigate("/"); }}>
+                <div className="flex aspect-square size-8 items-center justify-center rounded-lg">
                 <img src={LogoNoText} alt="logo" className="size-8"></img>
               </div>
               <div className="flex-1 text-left text-sm leading-tight">
                 <span className="truncate font-medium">GPS Tracker</span>
               </div>
+              </div>
+              
               
             </SidebarMenuButton>
           </DropdownMenuTrigger>
